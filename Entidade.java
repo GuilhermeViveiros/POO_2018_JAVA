e4e8067c9aac612852911fc3a1fb3cdb4730012d
Oliver Galt <gonca2372@gmail.com>
2018-02-20 11:48:24 +0000

    initial commit! @me

---
+++ Entidade.java
@@ -0,0 +1,32 @@
+
+/**
+ * Escreva a descrição da classe Entidade aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class Entidade
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private long nif;
+    private String nome;
+    private String mail;
+    private String morada;
+    private long pw;
+    private int key;
+    
+    public boolean verifymail( String x){
+    
+    }
+    
+    public boolean verifymorada( String x){
+    
+    }
+    
+    public boolean verifynif( long x, int key ){
+        
+    }
+    
+    
+    
+}
---
+++ Password.java
@@ -0,0 +1,17 @@
+
+/**
+ * Escreva a descrição da classe Password aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class Password
+{
+    private int key;
+    
+    public long code ( String pw ){
+        
+    }
+    
+    
+}
