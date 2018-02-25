11e365f29e463b32d44db3f1bb69f082853c8ff7
Angelo Sousa <a80813@gmail.com>
2018-02-25 16:00:48 +0000

    documentação pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -1,12 +1,12 @@
 public class Pessoa{
 
 	private Entidade info;
-	private int nrDAF;
-	private int nrFAF;
-	private float cF;
-	private string cAE;
+	private int nrDAF; // número de dependentes do agregado familiar
+	private int nrFAF; // números fiscais do agregado familiar
+	private float cF; // coeficiente fiscal
+	private String cAE; // códigos das atividades economicas
 
-	public Pessoa(){
+	public Pessoa(){ // cria uma pessoa do ponto zero(DEFAULT)
 
 		this.info = new Entidade();
 		this.nrDAF = 0;
@@ -15,23 +15,29 @@ public class Pessoa{
 		this.cAE = "Indefinido";
 
 	}
+	
+	// cria uma pessoa ao receber todos os dados necessários
 
-	public Pessoa(Entidade iD_P,int nrDAF_P, int nrFAF_P, float cF_P, string cAE_P){
+	public Pessoa(long ni_P, String nom_P, String mai_P, String morad_P, int nrDAF_P, int nrFAF_P, float cF_P, String cAE_P){
 
-		this.info = iD_P;
+		this.info = new Entidade(ni_P, nom_P, mai_P, morad_P);
 		this.nrDAF = nrDAF_P;
 		this.nrFAF = nrFAF_P;
 		this.cF = cF_P;
 		this.cAE = cAE_P;
 	}
 
-	public Pessoa(Pessoa iD){
+	// cria uma pessoa a partir de outra
 
+	public Pessoa(Pessoa iD){
+		this.info = iD.getInfo();
 		this.nrDAF = iD.getNrDAF();
 		this.nrFAF = iD.getNrFAF();
 		this.cF = iD.getCF();
 		this.cAE = iD.getCAE();
 	}
+
+	// getters
 
 	public int getNrDAF(){
 		return this.nrDAF;
@@ -45,11 +51,11 @@ public class Pessoa{
 		return this.cF;
 	}
 
-	public string getCAE(){
+	public String getCAE(){
 		return this.cAE;
 	}
 
-	public Entidade getID(){
-		return this.iD;
+	private Entidade getInfo(){
+		return this.info;
 	}
 }
