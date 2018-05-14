daa4116fee970d6cea3e9b67273badbc3db4e962
GuilhermeViveiros <a80524@alunos.uminho.pt>
2018-05-14 13:32:20 +0100

    a

---
+++ InvalidIntervalException.java
@@ -0,0 +1,14 @@
+public class InvalidIntervalException extends Exception
+{
+    public InvalidIntervalException(){
+        super();
+    }
+    
+    public InvalidIntervalException(String x){
+        super(x);
+    }
+    
+    public InvalidIntervalException(InvalidIntervalException x){
+        super(x);
+    }
+}
