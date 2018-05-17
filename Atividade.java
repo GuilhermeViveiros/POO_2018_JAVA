5da269fa6c39a7ae0871d21bdb093abcaa9412fc
Goncalo-Faria <gonca2372@gmail.com>
2018-05-17 16:51:21 +0100

    Adicionada a atividade a

---
+++ Atividade.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public interface Atividade
+{
+
+    public String getCodidigoActividade();
+    public String getNomeActividade();
+    public boolean areaDedusivel();
+    public double regraCalculo( Empresa x  ,LocalDate begin , LocalDate end);
+    public double regraCalculo( Pessoa x);
+    public Atividade clone();
+    public boolean equals(Object o);
+    public int hashCode();
+  
+ 
+}
